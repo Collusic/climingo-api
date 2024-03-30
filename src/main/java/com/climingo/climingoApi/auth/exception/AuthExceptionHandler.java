223a9e297e7ca051e039734c5947d77f6713b0d8
Bellroute <kjkun7631@gmail.com>
2024-03-30 23:19:27 +0900

    refactor: 패키지 명 변경 security -> auth

--- src/main/java/com/climingo/climingoApi/auth/exception/AuthExceptionHandler.java
+++ src/main/java/com/climingo/climingoApi/auth/exception/AuthExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class SecurityExceptionHandler {
+public class AuthExceptionHandler {
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(JwtException.class)
