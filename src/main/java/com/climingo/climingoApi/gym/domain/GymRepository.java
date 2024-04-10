c69415c109b376e4ead74d6727f4585096ae1a03
sunghyuki <sunghyuk1609@gmail.com>
2024-04-10 17:06:25 +0900

    feat: 암장 난이도 목록 조회 기능

--- src/main/java/com/climingo/climingoApi/gym/domain/GymRepository.java
+++ src/main/java/com/climingo/climingoApi/gym/domain/GymRepository.java
@@ -1,5 +1,6 @@
 package com.climingo.climingoApi.gym.domain;
 
+import com.climingo.climingoApi.gym.api.response.GradeResponse;
 import com.climingo.climingoApi.gym.api.response.GymSearchResponse;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,5 +13,8 @@ public interface GymRepository extends JpaRepository<Gym, Long> {
 
     @Query("select new com.climingo.climingoApi.gym.api.response.GymSearchResponse(g.id, g.address.address, g.address.zipCode, g.name) from Gym g where g.name like %:keyword%")
     List<GymSearchResponse> search(@Param("keyword") String keyword);
+
+    @Query("select new com.climingo.climingoApi.gym.api.response.GradeResponse(gd.id, gd.colorName) from Grade gd where gd.gym.id = :gymId")
+    List<GradeResponse> findGradesByGymId(@Param("gymId") Long gymId);
 
 }
