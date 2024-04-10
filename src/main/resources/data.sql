b5c8b24ed26bbdb5f8dfc8abec75dd910f76c7c5
sunghyuki <sunghyuk1609@gmail.com>
2024-04-10 16:10:30 +0900

    feat: data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO gym (id, latitude, longitude, address, name, zip_code) VALUES (1, 37.50416, 127.025142, '서울 강남구 강남대로 468', '클라이밍파크 신논현', NULL);
+
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (1, 1, 1, '노랑');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (2, 2, 1, '핑크');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (3, 3, 1, '파랑');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (4, 4, 1, '빨강');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (5, 5, 1, '보라');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (6, 6, 1, '갈색');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (7, 7, 1, '회색');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (8, 8, 1, '검정');
+INSERT INTO grade (id, order_num, gym_id, color_name) VALUES (9, 9, 1, '흰색');
