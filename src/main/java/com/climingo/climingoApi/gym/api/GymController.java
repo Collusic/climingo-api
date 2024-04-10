f36a0a4b3be954e79733eb8aef9088b98b22f9fb
sunghyuki <sunghyuk1609@gmail.com>
2024-04-10 17:06:25 +0900

    feat: 암장 난이도 목록 조회 기능

--- src/main/java/com/climingo/climingoApi/gym/api/GymController.java
+++ src/main/java/com/climingo/climingoApi/gym/api/GymController.java
@@ -1,11 +1,13 @@
 package com.climingo.climingoApi.gym.api;
 
+import com.climingo.climingoApi.gym.api.response.GradeResponse;
 import com.climingo.climingoApi.gym.api.response.GymSearchResponse;
 import com.climingo.climingoApi.gym.application.GymService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +20,12 @@ public class GymController {
     @GetMapping("/search/gyms")
     public ResponseEntity<List<GymSearchResponse>> search(@RequestParam("keyword") String keyword) {
         List<GymSearchResponse> response = gymService.search(keyword);
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/gyms/{gymId}/grades")
+    public ResponseEntity<List<GradeResponse>> findGradeList(@PathVariable Long gymId) {
+        List<GradeResponse> response = gymService.findGradeList(gymId);
         return ResponseEntity.ok().body(response);
     }
 
