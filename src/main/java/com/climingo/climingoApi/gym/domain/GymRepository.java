be20dbec668ddd79d9d958b891615fc4affb82a9
sunghyuki <sunghyuk1609@gmail.com>
2024-04-10 16:46:42 +0900

    feat: 암장명 검색 기능

--- src/main/java/com/climingo/climingoApi/gym/domain/GymRepository.java
+++ src/main/java/com/climingo/climingoApi/gym/domain/GymRepository.java
@@ -1,9 +1,16 @@
 package com.climingo.climingoApi.gym.domain;
 
+import com.climingo.climingoApi.gym.api.response.GymSearchResponse;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GymRepository extends JpaRepository<Gym, Long> {
+
+    @Query("select new com.climingo.climingoApi.gym.api.response.GymSearchResponse(g.id, g.address.address, g.address.zipCode, g.name) from Gym g where g.name like %:keyword%")
+    List<GymSearchResponse> search(@Param("keyword") String keyword);
 
 }
