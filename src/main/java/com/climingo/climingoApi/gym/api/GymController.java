1e7006da94a999d63d5adbcdaf52bc829032e7b5
sunghyuki <sunghyuk1609@gmail.com>
2024-04-10 16:46:42 +0900

    feat: 암장명 검색 기능

---
+++ src/main/java/com/climingo/climingoApi/gym/api/GymController.java
@@ -0,0 +1,24 @@
+package com.climingo.climingoApi.gym.api;
+
+import com.climingo.climingoApi.gym.api.response.GymSearchResponse;
+import com.climingo.climingoApi.gym.application.GymService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class GymController {
+
+    private final GymService gymService;
+
+    @GetMapping("/search/gyms")
+    public ResponseEntity<List<GymSearchResponse>> search(@RequestParam("keyword") String keyword) {
+        List<GymSearchResponse> response = gymService.search(keyword);
+        return ResponseEntity.ok().body(response);
+    }
+
+}
---
+++ src/main/java/com/climingo/climingoApi/gym/application/GymService.java
@@ -0,0 +1,19 @@
+package com.climingo.climingoApi.gym.application;
+
+import com.climingo.climingoApi.gym.api.response.GymSearchResponse;
+import com.climingo.climingoApi.gym.domain.GymRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class GymService {
+
+    private final GymRepository gymRepository;
+
+    public List<GymSearchResponse> search(String keyword) {
+        return gymRepository.search(keyword);
+    }
+
+}
