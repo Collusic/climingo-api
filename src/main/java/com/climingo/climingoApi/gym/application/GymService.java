99b141d3872d5aef304e8b73887cbf5305b7ddf1
sunghyuki <sunghyuk1609@gmail.com>
2024-04-10 17:06:25 +0900

    feat: 암장 난이도 목록 조회 기능

--- src/main/java/com/climingo/climingoApi/gym/application/GymService.java
+++ src/main/java/com/climingo/climingoApi/gym/application/GymService.java
@@ -1,5 +1,6 @@
 package com.climingo.climingoApi.gym.application;
 
+import com.climingo.climingoApi.gym.api.response.GradeResponse;
 import com.climingo.climingoApi.gym.api.response.GymSearchResponse;
 import com.climingo.climingoApi.gym.domain.GymRepository;
 import java.util.List;
@@ -14,6 +15,10 @@ public class GymService {
 
     public List<GymSearchResponse> search(String keyword) {
         return gymRepository.search(keyword);
+    }
+
+    public List<GradeResponse> findGradeList(Long gymId) {
+        return gymRepository.findGradesByGymId(gymId);
     }
 
 }
