59bb79168d1d9801bb09c2b72326167621d05483
Bellroute <kjkun7631@gmail.com>
2024-05-08 00:43:22 +0900

    feat: security filter chain에 jwtAuthenticationFilter 반영

--- src/main/java/com/climingo/climingoApi/global/config/SecurityConfig.java
+++ src/main/java/com/climingo/climingoApi/global/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.climingo.climingoApi.global.config;
 
 
+import com.climingo.climingoApi.auth.application.TokenService;
+import com.climingo.climingoApi.global.auth.JwtAuthenticationFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.PrintWriter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,13 +21,20 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final TokenService tokenService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManager authenticationManager = http.getSharedObject(
+            AuthenticationManagerBuilder.class).build();
+
         http
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(AbstractHttpConfigurer::disable)
@@ -30,13 +42,15 @@ public class SecurityConfig {
             .logout(AbstractHttpConfigurer::disable)
             .cors(AbstractHttpConfigurer::disable)
             .headers((headerConfig) -> headerConfig.frameOptions(FrameOptionsConfig::disable))
+            .authenticationManager(authenticationManager)
             .authorizeHttpRequests((authorizeRequests ->
                     // TODO 어떤 API에 인증/인가 처리를 둘 것인지 논의 필요
                     authorizeRequests
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/**").permitAll()
-                        .requestMatchers("/healthCheck", "/sign-up", "/sign-in", "/auth/members/exist").permitAll()
+                        .requestMatchers("/healthCheck", "/sign-up", "/sign-in", "/auth/members/exist")
+                        .permitAll()
                         .anyRequest().authenticated()
                 )
             )
@@ -44,7 +58,10 @@ public class SecurityConfig {
                 exceptionConfig
                     .authenticationEntryPoint(unauthorizedEntryPoint)
                     .accessDeniedHandler(accessDeniedHandler)
-            );
+            )
+            .addFilterAt(
+                new JwtAuthenticationFilter(authenticationManager, tokenService),
+                BasicAuthenticationFilter.class);
 
         return http.build();
     }
@@ -56,6 +73,7 @@ public class SecurityConfig {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
             String json = new ObjectMapper().writeValueAsString(fail);
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
             PrintWriter writer = response.getWriter();
             writer.write(json);
             writer.flush();
@@ -68,6 +86,7 @@ public class SecurityConfig {
             response.setStatus(HttpStatus.FORBIDDEN.value());
             String json = new ObjectMapper().writeValueAsString(fail);
             response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding("UTF-8");
             PrintWriter writer = response.getWriter();
             writer.write(json);
             writer.flush();
