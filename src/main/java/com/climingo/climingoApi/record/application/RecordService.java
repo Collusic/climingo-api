57504fedff8d7dffd2858abf316f0eeaf6486541
sunghyuki <sunghyuk1609@gmail.com>
2024-05-22 22:18:55 +0900

    feat: 기록 페이징 조회 현재 페이지 응답에 추가

--- src/main/java/com/climingo/climingoApi/record/application/RecordService.java
+++ src/main/java/com/climingo/climingoApi/record/application/RecordService.java
@@ -112,6 +112,7 @@ public class RecordService {
         return PageDto.<RecordResponse>builder()
                       .totalCount(recordPage.getTotalElements())
                       .resultCount(recordPage.getNumberOfElements())
+                      .pageNumber(page)
                       .isEnd(recordPage.isLast())
                       .contents(toRecordResponses(recordPage.getContent()))
                       .build();
