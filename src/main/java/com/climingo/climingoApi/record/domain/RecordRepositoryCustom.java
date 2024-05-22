e023baac8b0bf2cb56d0e9434cea6cf41ae36bb4
sunghyuki <sunghyuk1609@gmail.com>
2024-05-22 21:16:01 +0900

    feat: 기록 페이징 조회 구현

--- src/main/java/com/climingo/climingoApi/record/domain/RecordRepositoryCustom.java
+++ src/main/java/com/climingo/climingoApi/record/domain/RecordRepositoryCustom.java
@@ -1,9 +1,12 @@
 package com.climingo.climingoApi.record.domain;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
 
 public interface RecordRepositoryCustom {
 
     List<Record> findAllWithDetails(Long gymId, Long levelId, Long memberId);
+
+    Page<Record> findRecordPage(Long gymId, Long levelId, Long memberId, Integer page, Integer size);
 
 }
