0f1f2220a4de5264f05f10e215586e0dc21ff5c3
JongKeun Kim <kjkun7631@gmail.com>
2024-06-02 15:26:41 +0900

    [#60] 디스코드 웹훅 에러메시지 및 예외핸들러 버그 개선 (#63)
    * feat: 5xx 에러 내용 discord로 받을 수 있도록 처리
    - discord client 및 message 구조
    - logging filter 추가
    - discord 채널 및 웹훅 추가
    
    * chore: 참고 링크 추가
    
    * fix: aws에 배포된 서버에 기본 url(/)로 요청이 계속 들어와서 에러메시지가 계속 생기는 문제 해결
    
    * fix: base url(/) post 요청에 대한 응답이 500으로 넘어오는 문제 해결

--- src/main/java/com/climingo/climingoApi/message/error/ErrorAlertMessageProvider.java
+++ src/main/java/com/climingo/climingoApi/message/error/ErrorAlertMessageProvider.java
@@ -4,12 +4,8 @@ import com.climingo.climingoApi.message.DiscordClient;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,26 +27,27 @@ public class ErrorAlertMessageProvider {
     private ErrorAlertMessage createAlertMessage(Exception e, HttpServletRequest request) {
         String requestTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
             .format(LocalDateTime.now());
-        String xffHeader = request.getHeader("X-FORWARDED-FOR");
+
+        String originalDomain = request.getHeader("X-Forwarded-Host");
+        if (originalDomain == null || originalDomain.isEmpty()) {
+            originalDomain = request.getServerName();
+        }
 
         // TODO POST 요청 데이터를 읽기
         StringBuilder requestBody = new StringBuilder();
         try (BufferedReader reader = request.getReader()) {
             String line;
             while ((line = reader.readLine()) != null) {
-                requestBody.append(line);
+                requestBody.append(line.replace("\"", "\\\""));
             }
         } catch (IOException ex) {
             // 요청 본문을 읽는데 실패한 경우, 예외 처리
             requestBody.append("Error reading request body: ").append(ex.getMessage());
         }
 
-        StringWriter stringWriter = new StringWriter();
-        e.printStackTrace(new PrintWriter(stringWriter));
-
         return ErrorAlertMessage.builder()
             .requestTime(requestTime)
-            .requestIp(xffHeader == null ? request.getRemoteAddr() : xffHeader)
+            .requestIp(originalDomain)
             .requestUrl("[" + request.getMethod() + "] " + request.getRequestURL())
             .requestQuery(request.getQueryString() == null ? "" : request.getQueryString())
             .requestData(requestBody.toString())
