38b76543dc30511755d6a0bda6e8bd601e69a79b
Bellroute <kjkun7631@gmail.com>
2024-06-15 22:17:13 +0900

    fix: 에러메시지 json 직렬화 문제 해결

--- src/main/java/com/climingo/climingoApi/message/DiscordClient.java
+++ src/main/java/com/climingo/climingoApi/message/DiscordClient.java
@@ -2,10 +2,12 @@ package com.climingo.climingoApi.message;
 
 import static org.apache.commons.codec.CharEncoding.UTF_8;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.entity.ContentType;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
 public class DiscordClient {
@@ -21,10 +23,18 @@ public class DiscordClient {
     public void send(String url, DiscordMessage discordMessage) {
         try {
             HttpHeaders headers = new HttpHeaders();
-            headers.add(HttpHeaders.CONTENT_TYPE, String.valueOf(ContentType.APPLICATION_JSON));
+            headers.setContentType(MediaType.APPLICATION_JSON);
             headers.add(HttpHeaders.ACCEPT_ENCODING, UTF_8);
 
-            HttpEntity<String> requestEntity = new HttpEntity<>(discordMessage.toJson(), headers);
+            // ObjectMapper를 사용하여 DiscordMessage 객체를 JSON 문자열로 변환
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonMessage = objectMapper.writeValueAsString(discordMessage);
+
+            // HttpEntity 객체 생성
+            HttpEntity<String> requestEntity = new HttpEntity<>(jsonMessage, headers);
+
+
+//            HttpEntity<DiscordMessage> requestEntity = new HttpEntity<>(discordMessage, headers);
 
             RestTemplate restTemplate = new RestTemplate();
             restTemplate.exchange(url, HttpMethod.POST,  requestEntity, Void.class);
