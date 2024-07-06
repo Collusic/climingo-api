2966742ae258279b7bc1667ef0f9cdab56abf1f1
Bellroute <kjkun7631@gmail.com>
2024-07-06 14:09:43 +0900

    feat: localDateTime 한국시로 변경

--- src/main/java/com/climingo/climingoApi/ClimingoApiApplication.java
+++ src/main/java/com/climingo/climingoApi/ClimingoApiApplication.java
@@ -1,5 +1,7 @@
 package com.climingo.climingoApi;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -11,6 +13,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @PropertySource("classpath:application.yml")
 public class ClimingoApiApplication {
+
+    @PostConstruct
+    void postConstruct() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ClimingoApiApplication.class, args);
