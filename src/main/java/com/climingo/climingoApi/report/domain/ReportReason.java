582dfd82aa29d6da0309fe557daa6feaa8f5202e
Bellroute <kjkun7631@gmail.com>
2025-03-09 21:11:51 +0900

    update: 신고 사유 목록 수정

--- src/main/java/com/climingo/climingoApi/report/domain/ReportReason.java
+++ src/main/java/com/climingo/climingoApi/report/domain/ReportReason.java
@@ -8,14 +8,14 @@ import java.util.stream.Stream;
 
 @Getter
 public enum ReportReason {
-    IRRELEVANT_CONTENT("01", "클라이밍과 관련 없는 이미지/내용"),
-    OBSCENE_OR_ABUSIVE("02", "음란, 욕설, 비방 내용"),
+    IRRELEVANT_CONTENT("01", "서비스와 관련 없는 이미지/내용"),
+    OBSCENE_OR_ABUSIVE("02", "음란, 욕설, 혐오유발, 비방 내용"),
     ADVERTISING("03", "개인의 광고, 홍보성 내용"),
     PRIVACY_RISK("04", "개인정보 유출 위험"),
-    MISLEADING_HEALTH_INFO("05", "질병 예방·치료 관련 효능·효과 내용"),
-    SPAM("06", "게시글 도배"),
-    COPYRIGHT_INFRINGEMENT("07", "저작권, 초상권 침해"),
-    OTHER("99", "기타");
+    SPAM("05", "게시글 도배"),
+    COPYRIGHT_INFRINGEMENT("06", "저작권, 초상권 침해"),
+    OTHER("99", "기타")
+    ;
 
     private final String code;
     private final String description;
