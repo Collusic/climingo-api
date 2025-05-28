0993888d7857d674863d58a7e494d06dd008629b
Sehwan <abovenormal5023@gmail.com>
2025-05-28 18:03:21 +0900

    Refactor: swagger 설정 적용

--- src/main/java/com/climingo/climingoApi/record/api/RecordController.java
+++ src/main/java/com/climingo/climingoApi/record/api/RecordController.java
@@ -9,9 +9,14 @@ import com.climingo.climingoApi.record.api.response.PageDto;
 import com.climingo.climingoApi.record.api.response.RecordResponse;
 import com.climingo.climingoApi.record.application.RecordService;
 import com.climingo.climingoApi.record.domain.Record;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Min;
+
 import java.io.IOException;
 import java.util.Map;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name="RecordController", description = "영상 기록 api")
 @RequiredArgsConstructor
 @RestController
 public class RecordController {
@@ -31,6 +37,10 @@ public class RecordController {
     private final RecordService recordService;
 
     @PostMapping("/records")
+    @Operation(summary = "기록 생성", description = "사용자가 기록을 작성합니다.")
+    @Parameter(name = "gymId", description = "클라이밍장 id")
+    @Parameter(name = "leveId", description = "난이도 id")
+    @Parameter(name = "videoUrl", description = "영상 url")
     public ResponseEntity<Map<String, Long>> create(
             @RequestMember Member member,
             @RequestBody RecordCreateRequest request) throws IOException {
@@ -40,6 +50,10 @@ public class RecordController {
     }
 
     @PatchMapping("/records/{recordId}")
+    @Operation(summary = "기록 수정", description = "게시된 기록 수정합니다.(추후 수정할 필드가 생기면 추가예정)")
+    @Parameter(name = "gymId", description = "클라이밍장 id")
+    @Parameter(name = "levelId", description = "난이도 id")
+    @Parameter(name = "video", description = "영상")
     public ResponseEntity<Long> update(
             @RequestMember Member member,
             @PathVariable("recordId") Long recordId,
@@ -50,6 +64,8 @@ public class RecordController {
     }
 
     @DeleteMapping("/records/{recordId}")
+    @Operation(summary = "기록 삭제", description = "기록을 삭제합니다.")
+    @Parameter(name = "recordId", description = "기록(게시글) Id")
     public ResponseEntity<Void> delete(
             @RequestMember Member member,
             @PathVariable("recordId") Long recordId) {
@@ -58,6 +74,8 @@ public class RecordController {
     }
 
     @GetMapping("/records/{recordId}")
+    @Operation(summary = "기록 상세 조회", description = "1건의 기록을 조회합니다.")
+    @Parameter(name = "recordId", description = "클라이밍장 id")
     public ResponseEntity<RecordResponse> find(
             @RequestMember Member member,
             @PathVariable("recordId") Long recordId) {
@@ -74,6 +92,12 @@ public class RecordController {
 //    }
 
     @GetMapping("/records")
+    @Operation(summary = "기록 목록 조회", description = "페이징 조회")
+    @Parameter(name = "gymId", description = "클라이밍장 id")
+    @Parameter(name = "levelId", description = "난이도 id")
+    @Parameter(name = "memberId", description = "멤버 id")
+    @Parameter(name = "page", description = "페이지(페이지는 0페이지부터 시작)")
+    @Parameter(name = "size", description = "한 페이지에 조회할 데이터 개수")
     public ResponseEntity<PageDto<RecordResponse>> findPage(@RequestParam(value = "gymId", required = false) Long gymId,
                                                             @RequestParam(value = "levelId", required = false) Long levelId,
                                                             @RequestParam(value = "memberId", required = false) Long memberId,
@@ -86,6 +110,9 @@ public class RecordController {
     }
 
     @GetMapping("/myRecords")
+    @Operation(summary = "내 기록 목록 조회", description = "사용자의 기록들을 조회합니다.")
+    @Parameter(name = "page", description = "페이지(페이지는 0페이지부터 시작)")
+    @Parameter(name = "size", description = "한 페이지에 조회할 데이터 개수")
     public ResponseEntity<PageDto<MyRecordResponse>> findPageMy(@RequestMember Member member,
                                                                 @RequestParam(value = "page", required = false, defaultValue = "0") @Min(0) Integer page,
                                                                 @RequestParam(value = "size", required = false, defaultValue = "10") @Min(1) Integer size) {
