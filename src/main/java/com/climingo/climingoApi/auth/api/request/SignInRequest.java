7aa0026f4ca7a277be7300cb7d5f3ed37a17598b
David.Han <51263415+abovenormal@users.noreply.github.com>
2025-08-10 15:14:05 +0900

    Feat/137 applying swagger api (#140)
    * feat: swaggerConfig 파일 생성
    
    * Refactor: swagger 설정 추가
    
    * Refactor: swagger 설정 적용
    
    ---------
    
    Co-authored-by: sunghyuki <62830487+sunghyuki@users.noreply.github.com>
    Co-authored-by: JongKeun Kim <kjkun7631@gmail.com>

--- src/main/java/com/climingo/climingoApi/auth/api/request/SignInRequest.java
+++ src/main/java/com/climingo/climingoApi/auth/api/request/SignInRequest.java
@@ -2,6 +2,7 @@ package com.climingo.climingoApi.auth.api.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -16,9 +17,11 @@ public class SignInRequest {
     @JsonProperty("providerType")
     @Pattern(regexp = "^(kakao|apple)$", message = "providerType은 kakao와 apple만 유효합니다.")
     @NotNull
+    @Schema(description ="provider 타입",example = "kakao or apple", required = true)
     private final String providerType;
 
     @JsonProperty("providerToken")
     @NotNull
+    @Schema(description = "provider 유저 정보 조회용 토큰", example = "xxxxxx")
     private final String providerToken;
 }
