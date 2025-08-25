3f98f0837fef4e825855cfff4bbba05916cae7a4
David <abovenormal5023@gmail.com>
2025-08-25 14:53:35 +0900

    feat: 기록 생성 및 수정 시 암리치(physicalinfO) 정보를 추가 할 수 있게 수정

--- src/main/java/com/climingo/climingoApi/record/application/RecordService.java
+++ src/main/java/com/climingo/climingoApi/record/application/RecordService.java
@@ -6,6 +6,7 @@ import com.climingo.climingoApi.gym.domain.GymRepository;
 import com.climingo.climingoApi.level.domain.Level;
 import com.climingo.climingoApi.level.domain.LevelRepository;
 import com.climingo.climingoApi.member.domain.Member;
+import com.climingo.climingoApi.member.domain.PhysicalInfo;
 import com.climingo.climingoApi.record.api.request.RecordCreateRequest;
 import com.climingo.climingoApi.record.api.request.RecordUpdateRequest;
 import com.climingo.climingoApi.record.api.response.MyRecordResponse;
@@ -22,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -42,10 +44,10 @@ public class RecordService {
     @Transactional
     public Record createRecord(Member loginMember, RecordCreateRequest request) {
         Gym gym = gymRepository.findById(request.getGymId())
-                               .orElseThrow(() -> new EntityNotFoundException(request.getGymId() + "is not found"));
+                .orElseThrow(() -> new EntityNotFoundException(request.getGymId() + "is not found"));
 
         Level level = levelRepository.findById(request.getLevelId())
-                                     .orElseThrow(() -> new EntityNotFoundException(request.getLevelId() + "is not found"));
+                .orElseThrow(() -> new EntityNotFoundException(request.getLevelId() + "is not found"));
 
         String videoUrl = request.getVideoUrl();
         LocalDate climbedDate = request.getClimbDate() == null ? LocalDate.now(): request.getClimbDate();
@@ -58,6 +60,8 @@ public class RecordService {
             // TODO 썸네일 이미지 저장 안되었을 때 처리
         }
 
+        PhysicalInfo physicalInfo = loginMember.getPhysicalInfo();
+
         Record record = Record.builder()
                               .member(loginMember)
                               .gym(gym)
@@ -66,6 +70,7 @@ public class RecordService {
                               .videoUrl(videoUrl)
                               .thumbnailUrl(thumbnailImageUrl)
                               .climbDate(climbedDate)
+                              .physicalInfo(physicalInfo)
                               .build();
 
         return recordRepository.save(record);
@@ -74,20 +79,20 @@ public class RecordService {
     @Transactional
     public Record updateRecord(Member member, Long recordId, RecordUpdateRequest request) {
         Record record = recordRepository.findById(recordId)
-                                        .orElseThrow(() -> new EntityNotFoundException(recordId + "is not found"));
+                .orElseThrow(() -> new EntityNotFoundException(recordId + "is not found"));
 
         if (!record.isEditable(member)) {
             throw new ForbiddenException("다른 사용자가 업로드한 record는 수정할 수 없음");
         }
 
         Gym gym = gymRepository.findById(request.getGymId())
-                               .orElseThrow(() -> new EntityNotFoundException(request.getGymId() + "is not found"));
+                .orElseThrow(() -> new EntityNotFoundException(request.getGymId() + "is not found"));
 
         Level level = levelRepository.findById(request.getLevelId())
-                                     .orElseThrow(() -> new EntityNotFoundException(request.getLevelId() + "is not found"));
+                .orElseThrow(() -> new EntityNotFoundException(request.getLevelId() + "is not found"));
 
         // TODO: origin 영상 데이터와 updated 영상 데이터가 다른걸 어떻게 알 것인가?
-        record.update(gym, level, null);
+        record.update(gym, level, null, member);
         record.updateClimbDate(request.getClimbDate());
 
         return record;
@@ -96,7 +101,7 @@ public class RecordService {
     @Transactional
     public void deleteRecord(Member member, Long recordId) {
         Record record = recordRepository.findById(recordId)
-                                        .orElseThrow(() -> new NoSuchElementException("존재하지 않는 기록입니다."));
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 기록입니다."));
 
         if (!record.isEditable(member)) {
             throw new ForbiddenException("다른 사용자가 업로드한 record는 삭제할 수 없음");
@@ -108,7 +113,7 @@ public class RecordService {
     @Transactional(readOnly = true)
     public RecordResponse readRecord(Member member, Long recordId) {
         Record record = recordRepository.findByIdWithMember(recordId)
-                                        .orElseThrow(() -> new EntityNotFoundException(recordId + "is not found"));
+                .orElseThrow(() -> new EntityNotFoundException(recordId + "is not found"));
 
         return new RecordResponse(member, record);
     }
@@ -131,13 +136,13 @@ public class RecordService {
         Page<Record> recordPage = recordRepository.findRecordPage(requestMember, gymId, levelId, memberId, page, size);
 
         return PageDto.<RecordResponse>builder()
-                      .totalCount(recordPage.getTotalElements())
-                      .resultCount(recordPage.getNumberOfElements())
-                      .totalPage((int) Math.ceil((double) recordPage.getTotalElements() / size))
-                      .page(page)
-                      .isEnd(recordPage.isLast())
-                      .contents(toRecordResponses(recordPage.getContent()))
-                      .build();
+                .totalCount(recordPage.getTotalElements())
+                .resultCount(recordPage.getNumberOfElements())
+                .totalPage((int) Math.ceil((double) recordPage.getTotalElements() / size))
+                .page(page)
+                .isEnd(recordPage.isLast())
+                .contents(toRecordResponses(recordPage.getContent()))
+                .build();
     }
 
     @Transactional(readOnly = true)
@@ -145,25 +150,25 @@ public class RecordService {
         Page<Record> myRecordPage = recordRepository.findMyRecordPage(memberId, page, size);
 
         return PageDto.<MyRecordResponse>builder()
-                      .totalCount(myRecordPage.getTotalElements())
-                      .resultCount(myRecordPage.getNumberOfElements())
-                      .totalPage((int) Math.ceil((double) myRecordPage.getTotalElements() / size))
-                      .page(page)
-                      .isEnd(myRecordPage.isLast())
-                      .contents(toMyRecordResponses(myRecordPage.getContent()))
-                      .build();
+                .totalCount(myRecordPage.getTotalElements())
+                .resultCount(myRecordPage.getNumberOfElements())
+                .totalPage((int) Math.ceil((double) myRecordPage.getTotalElements() / size))
+                .page(page)
+                .isEnd(myRecordPage.isLast())
+                .contents(toMyRecordResponses(myRecordPage.getContent()))
+                .build();
     }
 
     private List<RecordResponse> toRecordResponses(List<Record> records) {
         return records.stream()
-                      .map(record -> new RecordResponse(record.getMember(), record))
-                      .collect(Collectors.toList());
+                .map(record -> new RecordResponse(record.getMember(), record))
+                .collect(Collectors.toList());
     }
 
     private List<MyRecordResponse> toMyRecordResponses(List<Record> records) {
         return records.stream()
-                      .map(record -> new MyRecordResponse(record, record.getGym(), record.getLevel()))
-                      .collect(Collectors.toList());
+                .map(record -> new MyRecordResponse(record, record.getGym(), record.getLevel()))
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/climingo/climingoApi/record/domain/Record.java
+++ src/main/java/com/climingo/climingoApi/record/domain/Record.java
@@ -4,14 +4,8 @@ import com.climingo.climingoApi.global.domain.BaseTimeEntity;
 import com.climingo.climingoApi.gym.domain.Gym;
 import com.climingo.climingoApi.level.domain.Level;
 import com.climingo.climingoApi.member.domain.Member;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.climingo.climingoApi.member.domain.PhysicalInfo;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -53,10 +47,13 @@ public class Record extends BaseTimeEntity {
 
     private String content;
 
+    @Embedded
+    private PhysicalInfo physicalInfo;
+
     private boolean blocked;
 
     @Builder
-    public Record(Long id, Member member, Level level, Gym gym, String videoUrl, String thumbnailUrl, String content, LocalDate climbDate) {
+    public Record(Long id, Member member, Level level, Gym gym, String videoUrl, String thumbnailUrl, String content, LocalDate climbDate,  PhysicalInfo physicalInfo) {
         this.id = id;
         this.member = member;
         this.level = level;
@@ -66,15 +63,18 @@ public class Record extends BaseTimeEntity {
         this.content = content;
         this.climbDate = climbDate;
         this.blocked = false;
+        this.physicalInfo = physicalInfo;
     }
 
-    public void update(Gym gym, Level level, String videoUrl) {
+    public void update(Gym gym, Level level, String videoUrl, Member loginMember) {
         if (!gym.getId().equals(this.gym.getId())) {
             this.gym = gym;
         }
         if (!level.getId().equals(this.level.getId())) {
             this.level = level;
         }
+
+        this.physicalInfo = loginMember.getPhysicalInfo();
         // TODO: origin 영상 데이터와 updated 영상 데이터가 다른걸 어떻게 알 것인가?
     }
 
