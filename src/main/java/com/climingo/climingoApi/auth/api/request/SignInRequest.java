dca355ede069474d6f7647d457c6a8310039bde1
Bellroute <kjkun7631@gmail.com>
2025-10-28 21:40:31 +0900

    feat: 찍볼 목록 조회 기능 추가

--- src/main/java/com/climingo/climingoApi/auth/api/request/SignInRequest.java
+++ src/main/java/com/climingo/climingoApi/auth/api/request/SignInRequest.java
@@ -3,7 +3,6 @@ package com.climingo.climingoApi.auth.api.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
--- src/main/java/com/climingo/climingoApi/jjikboul/api/JjikboulController.java
+++ src/main/java/com/climingo/climingoApi/jjikboul/api/JjikboulController.java
@@ -8,6 +8,8 @@ import com.climingo.climingoApi.jjikboul.application.JjikboulDeleteUseCase;
 import com.climingo.climingoApi.jjikboul.application.JjikboulQueryUseCase;
 import com.climingo.climingoApi.jjikboul.application.response.JjikboulResponse;
 import com.climingo.climingoApi.member.domain.Member;
+import com.climingo.climingoApi.record.api.response.PageDto;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,20 @@ public class JjikboulController {
     private final JjikboulCreateUseCase jjikboulCreateUseCase;
     private final JjikboulQueryUseCase jjikboulQueryUseCase;
     private final JjikboulDeleteUseCase jjikboulDeleteUseCase;
+
+    @GetMapping("/jjikbouls")
+    public ResponseEntity<PageDto<JjikboulResponse>> getJjikbouls(
+            @RequestMember Member member,
+            @RequestParam(value = "gymId", required = false) Long gymId,
+            @RequestParam(value = "levelId", required = false) Long levelId,
+            @RequestParam(value = "memberId", required = false) Long memberId,
+            @RequestParam(value = "page", required = false, defaultValue = "0") @Min(0) Integer page,
+            @RequestParam(value = "size", required = false, defaultValue = "10") @Min(1) Integer size
+    ) {
+        PageDto<JjikboulResponse> jjikboulResponsePageDto = jjikboulQueryUseCase.readJjikbouls(member, gymId, levelId, memberId, page, size);
+
+        return ResponseEntity.ok().body(jjikboulResponsePageDto);
+    }
 
     @GetMapping("/jjikbouls/{jjikboulId}")
     public ResponseEntity<JjikboulResponse> getJjikboul(
