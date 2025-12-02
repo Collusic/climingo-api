216218671752ee97db809c97cb27683eb33d7f8d
JongKeun Kim <kjkun7631@gmail.com>
2025-12-02 22:24:52 +0900

    [#150] 찍볼 목록 조회 api 구현 (#151)
    * feat: 찍볼 목록 조회 기능 추가
    
    * feat: 빌드 실패 이슈 해결
    
    * Update github-action-ci.yml
    
    * feat: 빌드 실패 이슈 해결

--- src/main/java/com/climingo/climingoApi/member/api/request/UpdateNicknameRequest.java
+++ src/main/java/com/climingo/climingoApi/member/api/request/UpdateNicknameRequest.java
@@ -1,17 +1,21 @@
 package com.climingo.climingoApi.member.api.request;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
-@JsonDeserialize(builder = UpdateNicknameRequest.UpdateNicknameRequestBuilder.class)
 public class UpdateNicknameRequest {
 
     @JsonProperty("nickname")
     @Size(min = 2, max = 16)
     private final String nickname;
+
+    @JsonCreator
+    public UpdateNicknameRequest(@JsonProperty("nickname") String nickname) {
+        this.nickname = nickname;
+    }
 }
